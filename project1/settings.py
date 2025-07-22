77989d3ea5beda0b3eee538e8cb3f3771ed4a22e
Yeruva Koushik Reddy <130329397+koushik21-design@users.noreply.github.com>
2025-07-22 23:34:07 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR /'project1'/'static'
 ]
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
